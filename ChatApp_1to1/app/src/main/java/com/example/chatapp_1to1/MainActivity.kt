a1f52f51a9063562e5eccba91058a9db37ea9f35
Hyeon6790 <yoa24007@naver.com>
2025-05-14 15:55:05 +0900

    시작화면 초기세팅 리셋

--- ChatApp_1to1/app/src/main/java/com/example/chatapp_1to1/MainActivity.kt
+++ ChatApp_1to1/app/src/main/java/com/example/chatapp_1to1/MainActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)  // 일단 정리겸 처음 시작 로그인 페이지 해둠 
+        setContentView(R.layout.activity_login)  // 일단 정리겸 처음 시작 로그인 페이지 해둠
     }
 }
