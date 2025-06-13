7be35c3e0cd95575057739481016a8475ba48d27
kimnaa <aapink6861@gmail.com>
2025-06-13 14:19:57 +0900

    누락된 부분 추가
    코드 연결 기능에서 누락 된 부분 추가

--- ChatApp_1to1/app/src/main/java/com/example/chatapp_1to1/PlantCareActivity.kt
+++ ChatApp_1to1/app/src/main/java/com/example/chatapp_1to1/PlantCareActivity.kt
@@ -1,7 +1,5 @@
 package com.example.chatapp_1to1
 
-import android.util.Log
-
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
@@ -47,9 +45,32 @@ class PlantCareActivity : AppCompatActivity() {
         setContentView(R.layout.activity_plant_care)
 
         findViewById<ImageView>(R.id.ivSpeechBubble).setOnClickListener {
+            val currentUser = FirebaseAuth.getInstance().currentUser
+            if (currentUser == null) {
+                Toast.makeText(this, "로그인 정보가 없습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val currentUid = currentUser.uid
+            val myUserRef = FirebaseDatabase.getInstance().getReference("users").child(currentUid)
 
-            startActivity(Intent(this, CodeInputActivity::class.java))
+            myUserRef.child("roomId").addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val myRoomId = snapshot.getValue(String::class.java)
+                    if (myRoomId.isNullOrEmpty()) {
+                        startActivity(Intent(this@PlantCareActivity, CodeInputActivity::class.java))
+                    } else {
+                        val intent = Intent(this@PlantCareActivity, ChatActivity::class.java)
+                        intent.putExtra("roomId", myRoomId)
+                        startActivity(intent)
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(this@PlantCareActivity, "데이터베이스 오류: ${error.message}", Toast.LENGTH_SHORT).show()
+                }
+            })
         }
+
 
         findViewById<ImageView>(R.id.btnMenu).setOnClickListener {
             showLogoutDialog(this)
